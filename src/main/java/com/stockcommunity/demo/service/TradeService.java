704383249bec5ded5f9c1980cfae7ff6ff0c8b92
bertramwg393-cmd <bertramwg393@gmail.com>
2026-08-01 22:32:50 +0800

    完成 TradeService，補上 findTradesByOrderId/findTradesByStockCode

--- src/main/java/com/stockcommunity/demo/service/TradeService.java
+++ src/main/java/com/stockcommunity/demo/service/TradeService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class TradeService {
@@ -33,11 +35,15 @@ public class TradeService {
 
     // 查某筆訂單相關的所有成交紀錄，不管這筆單當初是買方還是賣方都查得到
     public List<Trade> findTradesByOrderId(Long orderId) {
+        List<Trade> asBuyer = tradeRepository.findByBuyOrderId(orderId);
+        List<Trade> asSeller = tradeRepository.findBySellOrderId(orderId);
 
+        return Stream.concat(asBuyer.stream(), asSeller.stream())
+                .collect(Collectors.toList());
     }
 
     // 查某檔股票的所有成交紀錄
-
-
-
+    public List<Trade> findTradesByStockCode(String stockCode) {
+        return tradeRepository.findByStockCode(stockCode);
+    }
 }
